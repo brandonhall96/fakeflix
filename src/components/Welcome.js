490e942a73312cb73ea332f4256c799b6e38fd18
brandonhall96 <hall.bran96@gmail.com>
2021-06-28 16:33:48 -0400

    started api on welcome page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,9 +1,31 @@
 import React from 'react';
 
 const Welcome = () => {
+
+
+
+// Api call to database will be here
+const [error, setError] = useState(null);
+const [movieData, setMovieData] = useState([]);
+const [isLoaded, setIsLoaded] = useState(false);
+    
+useEffect(() =>{
+    let url = CONNECTION_URI+'/api/movie'
+    setAuthToken(localStorage.getItem("jwtToken"))
+    axios.get(url)
+    .then((res) =>{
+        setMovieData(res.data.rockets)
+        
+    })
+}, [])
+
+
+
+//we will need to use a .map to itterate and render all data needed
+
     return (
         <div>
-            <h1>Welcome to the thunderdome</h1>
+            <h1>All the Movies will be displayed hear from our own api call</h1>
         </div>
     )
 }
