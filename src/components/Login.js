edd121c1c766cc26f59f817c3aada1d979f4751f
brandonhall96 <hall.bran96@gmail.com>
2021-07-02 15:21:54 -0400

    work on read me and cleand login and signup

--- src/components/Login.js
+++ src/components/Login.js
@@ -44,7 +44,7 @@ const Login = (props) => {
 
     return (
         <div className="row mt-4">
-            <div className="col-md-7 offset-md-3">
+            <div className="col-md-6 offset-md-3">
                 <div className="card card-body">
                     <h2 className="py-2">Login</h2>
                     <form onSubmit={handleSubmit}>
